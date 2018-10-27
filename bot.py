14674adfbf2bdc25daff6ca1acfe453cd5a161bb
paulin-mipt <paulin.mipt@gmail.com>
2018-10-28 00:17:37 +0300

    try more webhook heroku

--- bot.py
+++ bot.py
@@ -94,6 +94,10 @@ def main():
         os.makedirs('./data')
     
     updater = Updater(TOKEN)
+    dp = updater.dispatcher
+    dp.add_handler(MessageHandler(Filters.audio | Filters.document, audio_echo))
+    dp.add_handler(MessageHandler(Filters.voice, voice_echo))
+    dp.add_error_handler(error)
     
     if 'ON_HEROKU' in os.environ:
         port = int(os.environ['PORT'])
@@ -106,13 +110,8 @@ def main():
                               port=port,
                               url_path=TOKEN,
                               webhook_url='https://{}.{}:{}/{}'.format(name, domain, port, TOKEN))
-
-    dp = updater.dispatcher
-    dp.add_handler(MessageHandler(Filters.audio | Filters.document, audio_echo))
-    dp.add_handler(MessageHandler(Filters.voice, voice_echo))
-    dp.add_error_handler(error)
-
-    updater.start_polling()
+    else:
+        updater.start_polling()
     updater.idle()
 
 
